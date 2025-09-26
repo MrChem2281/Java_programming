f5d60a4d2f2f68c031f3dbce7493e7181bdd12a6
MrChem2281 <rus.rfarxullin.farxullin@mail.ru>
2025-09-26 18:16:03 +0300

    +product +postgresql

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -14,48 +14,67 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
 
 @RestController
 public class ProductController {
-    @GetMapping("/products")
-    public List<Product> getProducts() {
-        return productService.getAll();
-    }
+
     private final ProductService productService;
     
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
-
     private List<Product> products=new ArrayList<>(Arrays.asList(new Product(1l, "Сок", 130)));
+
+
+    @GetMapping("/products")
+    public List<Product> getProducts(@RequestParam(required = false) String title) {
+        
+        if (title == null){
+            return productService.getAll();
+        }
+        return productService.getByTitle(title);
+    }
     
     @PostMapping("/products")
     public ResponseEntity<Product> postProducts(@RequestBody @Valid Product product) {
-        //TODO: process POST request
-        // product.setId((long)products.size()+1);
-        // products.add(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.create(product));
     }
 
     @GetMapping("/products/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable Long id) {
-        for (Product product : products) {
-            if(product.getId().equals(id)){
-                return ResponseEntity.ok(product);
-            }
+        Product product = productService.getById(id);
+        if (product!=null){
+            return ResponseEntity.ok(product);
         }
         return ResponseEntity.notFound().build();
-        // return new String();
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody @Valid Product product) {
+        Product edit = productService.update(id, product);
+        if (edit!=null){
+            return ResponseEntity.ok(edit);
+        }
+        return ResponseEntity.notFound().build();
     }
     
-    
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        if (productService.deleteById(id)){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,6 +1,15 @@
 package com.example.demo.model;
 
+import org.hibernate.annotations.Collate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,10 +17,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     @NotBlank
+    @Column(nullable = false, unique = true, length = 100)
+    @Size(min = 2, max = 100)
     private String title;
+
+    @Column(nullable = false)
+    @Min(1)
     private int cost;
 }
